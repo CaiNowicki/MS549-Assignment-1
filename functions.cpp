d5c6154a724368287450289823d0b199c07c32af
Cai Nowicki <cai.nowicki@gmail.com>
2024-11-10 13:07:29 -0500

    Add project files.

---
+++ functions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "functions.h"
+using namespace std;
+
+int addition(int a, int b) {
+	return a + b;
+}
+
+int subtraction(int a, int b) {
+	return a - b;
+}
+
+int multiplication(int a, int b) {
+	return a * b;
+}
+
+double division(double a, double b) {
+	if (b == 0) {
+		cerr << "Error: Division by 0 is undefined" << endl;
+		showMenu();
+	}
+	return a / b;
+}
+
+void showMenu() {
+	cout << "\n**Menu**\n";
+	cout << "1. Addition" << endl;
+	cout << "2. Subtraction" << endl;
+	cout << "3. Multiplication" << endl;
+	cout << "4. Division" << endl;
+	cout << "5. Exit Program" << endl;
+}
